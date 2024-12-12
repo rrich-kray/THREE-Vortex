2a0932b8bc29d8f1dbfc5f74bccb249764b63bc5
Ryan Kray <rrich.kray.93@gmail.com>
2024-12-12 14:04:47 -0500

    refactored into classes

--- src/main.ts
+++ src/main.ts
@@ -101,139 +101,37 @@ const setup = new Setup();
 
 setup.Tick();
 
-class ObjectInitializer
+class Particles
 {
-  private Scene: THREE.Scene;
   private Camera: THREE.PerspectiveCamera;
+  private Scene: THREE.Scene;
   private Renderer: THREE.WebGLRenderer;
+  private ParticleHelpers: ParticleHelpers;
+  private ShapeGeometryRotationRate: number;
+  private CameraRotationRate: number;
 
-  constructor(scene: THREE.Scene, camera: THREE.PerspectiveCamera, renderer: THREE.WebGLRenderer)
+  constructor(
+    camera: THREE.PerspectiveCamera, 
+    scene: THREE.Scene, 
+    renderer: THREE.WebGLRenderer,
+    particleHelpers: ParticleHelpers,
+    shapeGeometryRotationRate: number,
+    cameraRotationRate: number)
   {
-    this.Scene = scene;
     this.Camera = camera;
+    this.Scene = scene;
     this.Renderer = renderer;
+    this.ParticleHelpers = particleHelpers;
+    this.ShapeGeometryRotationRate = shapeGeometryRotationRate;
+    this.CameraRotationRate = cameraRotationRate;
   }
 
-  public InstancesWithinTunnel()
-  {
-    const cubeDimensions = {
-      width: 0.5,
-      height: 0.5,
-      depth: 0.5
-    }
-
-    const cubeInstanceCount = 50;
-    const cubeInstancedMaterial: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 'white' });
-    const cubeInstancedGeometry: THREE.BoxGeometry = new THREE.BoxGeometry(cubeDimensions.width, cubeDimensions.height, cubeDimensions.depth);
-    const cubeInstancedMesh = new THREE.InstancedMesh(cubeInstancedGeometry, cubeInstancedMaterial, cubeInstanceCount);
-
-    const dummy = new THREE.Object3D();
-
-    for (let i=0; i<=cubeInstanceCount; i++)
-    {
-      const angle = Math.random() * Math.PI * 6;
-      const radius = 3 + Math.random() * 1;
-      const x = Math.sin(angle) * radius;
-      const y = Math.cos(angle) * radius;
-      const z = Math.cos(angle) * radius;
-
-      dummy.position.x = x;
-      dummy.position.y = y;
-      dummy.position.z = z;
-      dummy.updateMatrix();
-
-      cubeInstancedMesh.setMatrixAt(i, dummy.matrix);
-    } 
-    // cubeInstancedMesh.rotateZ(10);
-    this.Scene.add(cubeInstancedMesh);
-  }
-
-  public InstanceShape()
-  {
-    const sphereDimensions = {
-      radius: 0.05,
-      widthSegments: 8
-    }
-
-    const torusGeometry: THREE.TorusGeometry = new THREE.TorusGeometry(5, 1, 50, 300);
-    const torusPositions = torusGeometry.getAttribute('position');
-    torusGeometry.rotateY(7.85);
-    
-    this.Camera.position.set(0, 5, 2);
-    
-    const sphereInstanceCount = torusPositions.count;
-    const sphereInstancedMaterial: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 'red' });
-    const sphereInstancedGeometry: THREE.SphereGeometry = new THREE.SphereGeometry(sphereDimensions.radius, sphereDimensions.widthSegments);
-    const sphereInstancedMesh = new THREE.InstancedMesh(sphereInstancedGeometry, sphereInstancedMaterial, sphereInstanceCount);
-
-    const vertex = new THREE.Vector3();
-    const object3d = new THREE.Object3D();
-
-    for (let i=0; i<=sphereInstanceCount; i++)
-    {
-      vertex.fromBufferAttribute(torusPositions, i);
-      object3d.position.x = vertex.x;
-      object3d.position.y = vertex.y;
-      object3d.position.z = vertex.z;
-      object3d.updateMatrix();
-
-      sphereInstancedMesh.setMatrixAt(i, object3d.matrix);
-    } 
-    this.Scene.add(sphereInstancedMesh);
-
-    this.Renderer.setAnimationLoop(() => 
-    {
-      torusGeometry.rotateZ(0.025);
-      this.Camera.rotateZ(0.001);
-
-      const newTorusPositions = torusGeometry.getAttribute('position');
-
-      for (let i=0; i<=sphereInstanceCount-1; i++)
-      {
-        vertex.fromBufferAttribute(newTorusPositions, i);
-        
-        object3d.position.x = vertex.x;
-        object3d.position.y = vertex.y;
-        object3d.position.z = vertex.z;
-
-        object3d.updateMatrix();
-
-        sphereInstancedMesh.setMatrixAt(i, object3d.matrix);
-      }
-    });
-
-  }
-
-  public ParticleShape()
+  public Generate(): void
   {
     const textureLoader = new THREE.TextureLoader();
     const particlesTexture = textureLoader.load('../static/textures/particles/5.png');
-    const torusGeometry: THREE.TorusGeometry = new THREE.TorusGeometry(5, 1, 75, 375);
-    const particlesGeometry = new THREE.BufferGeometry();
-    const torusVertexPositions = torusGeometry.getAttribute('position');
-    const particleCount: number = torusVertexPositions.count*3;
-    const particlePositions: Float32Array = new Float32Array(particleCount);
-    const particlesColors: Float32Array = new Float32Array(particleCount);
-    const vertex = new THREE.Vector3();
-
-    let j = 0;
-    for (let i=0; i<=particleCount; i+=3)
-    {
-      vertex.fromBufferAttribute(torusVertexPositions, j);
-      particlePositions[i] = vertex.x;
-      particlePositions[i+1] = vertex.y;
-      particlePositions[i+2] = vertex.z;
-
-      particlesColors[i] = 212;
-      particlesColors[i+1] = 70;
-      particlesColors[i+2] = 40;
-
-      j++;
-    }
-
-    particlesGeometry.setAttribute('position', new THREE.BufferAttribute(particlePositions, 3));
-    particlesGeometry.setAttribute('color', new THREE.BufferAttribute(particlesColors, 3));
-
+    const particlesBaseColor = new THREE.Color(0.75, 0.5, 0.25);
+    const particlesShape = new THREE.TorusGeometry(5, 1, 75, 375);
     const particlesMaterial = new THREE.PointsMaterial({
       sizeAttenuation: true,
       color: "white",
@@ -247,79 +145,111 @@ class ObjectInitializer
       vertexColors: true
     });
 
-    this.Camera.position.z = 0;
-    this.Camera.position.y = 5.25;
-    const cameraVector = new THREE.Vector3(Math.PI * 2.5, 1, 0.5);
-    setup.Camera.lookAt(cameraVector);
+    const particles = this.ParticleHelpers.GenerateShape(particlesShape, particlesMaterial, particlesBaseColor);
 
-    const particles = new THREE.Points(particlesGeometry, particlesMaterial);
-    this.Scene.add(particles);
-
-    let torusRotationRate = 0.0025;
-
-    function render(renderer: THREE.WebGLRenderer, camera: THREE.Camera)
-    {
-      renderer.setAnimationLoop(() => 
-      {
-        torusGeometry.rotateZ(torusRotationRate);
-        camera.rotateZ(0.001);
-  
-        const newTorusPositions = torusGeometry.getAttribute('position');
-  
-        let j = 0;
-        for (let i=0; i<=particleCount; i+=3)
-        {
-          vertex.fromBufferAttribute(newTorusPositions, j);
-          particlePositions[i] = vertex.x;
-          particlePositions[i+1] = vertex.y;
-          particlePositions[i+2] = vertex.z;
-  
-          j++;
-        }
-  
-        particlesGeometry.setAttribute('position', new THREE.BufferAttribute(particlePositions, 3));
-      });
-    }
-
-    render(setup.Renderer, setup.Camera);
+    this.AdjustCamera();
+    this.RenderAnimation(
+      particles.geometry,
+      particlesShape,
+      new Float32Array(particles.geometry.getAttribute('position').array)
+    );
 
     const colors: THREE.Color[] = [
-      // new THREE.Vector3().set(0, 0, 0),
       new THREE.Color().setRGB(0.5, 0.25, 0),
       new THREE.Color().setRGB(0.25, 0.5, 1),
       new THREE.Color().setRGB(0.1, 0.333, 0.22)
     ]
 
-    const setParticleColorsWithDelay = (colorIndex: number) => 
-    {
-      let delay: number = 0;
-      for (let i=0; i<=particleCount; i+=3)
-      {
-        setTimeout(() => 
-        {
-          particlesColors[i] = colors[colorIndex].r;
-          particlesColors[i+1] = colors[colorIndex].g;
-          particlesColors[i+2] = colors[colorIndex].b;
-          particlesGeometry.setAttribute('color', new THREE.BufferAttribute(particlesColors, 3));
-        }, delay);
-        delay += 0.025;
-      }
-    }
+    this.RegisterEventListeners(
+      particlesShape,
+      new Float32Array(particles.geometry.getAttribute('color').array),
+      colors,
+      particles.geometry
+    )
+  }
 
-    const setParticlesColorsWithoutDelay = (colorIndex: number) => 
+  private AdjustCamera()
+  {
+    const cameraVector = new THREE.Vector3(Math.PI * 2.5, 1, 0.5);
+    this.Camera.position.z = 0;
+    this.Camera.position.y = 5.25;
+    this.Camera.lookAt(cameraVector);
+  }
+
+  private RenderAnimation(
+    particlesGeometry: THREE.BufferGeometry,
+    shapeGeometry: THREE.BufferGeometry,
+    particlePositions: Float32Array)
+  {
+    const vertex = new THREE.Vector3;
+    this.Renderer.setAnimationLoop(() => 
     {
-      for (let i=0; i<=particleCount; i+=3)
-      { 
-        particlesColors[i] =  colors[colorIndex].r;
+      shapeGeometry.rotateZ(this.ShapeGeometryRotationRate);
+      this.Camera.rotateZ(this.CameraRotationRate);
+
+      const newTorusPositions = shapeGeometry.getAttribute('position');
+
+      let j = 0;
+      for (let i=0; i<=particlePositions.length-1; i+=3)
+      {
+        vertex.fromBufferAttribute(newTorusPositions, j);
+        particlePositions[i] = vertex.x;
+        particlePositions[i+1] = vertex.y;
+        particlePositions[i+2] = vertex.z;
+
+        j++;
+      }
+
+      particlesGeometry.setAttribute('position', new THREE.BufferAttribute(particlePositions, 3));
+    });
+  }
+
+  private SetParticleColorsWithDelay = (
+    colorIndex: number,
+    particlesColors: Float32Array,
+    colors: THREE.Color[],
+    particlesGeometry: THREE.BufferGeometry) => 
+  {
+    let delay: number = 0;
+    for (let i=0; i<=particlesColors.length-1; i+=3)
+    {
+      setTimeout(() => 
+      {
+        particlesColors[i] = colors[colorIndex].r;
         particlesColors[i+1] = colors[colorIndex].g;
         particlesColors[i+2] = colors[colorIndex].b;
-      }
-      particlesGeometry.setAttribute('color', new THREE.BufferAttribute(particlesColors, 3));
+        particlesGeometry.setAttribute('color', new THREE.BufferAttribute(particlesColors, 3));
+      }, delay);
+      delay += 0.025;
     }
+  }
+  
+  private SetParticlesColorsWithoutDelay = (
+    colorIndex: number,
+    particlesColors: Float32Array,
+    colors: THREE.Color[],
+    particlesGeometry: THREE.BufferGeometry) => 
+  {
+    for (let i=0; i<=particlesColors.length-1; i+=3)
+    { 
+      particlesColors[i] =  colors[colorIndex].r;
+      particlesColors[i+1] = colors[colorIndex].g;
+      particlesColors[i+2] = colors[colorIndex].b;
+    }
+    particlesGeometry.setAttribute('color', new THREE.BufferAttribute(particlesColors, 3));
+  }
 
+  private RegisterEventListeners(
+    particlesShapeGeometry: THREE.BufferGeometry,
+    particlesColors: Float32Array,
+    colors: THREE.Color[],
+    particlesGeometry: THREE.BufferGeometry
+  )
+  {
     let intervalId: number;
     let colorIndex = 0;
     const dropdownBtns: NodeListOf<Element> = document.querySelectorAll('.startBtn')!;
+
     dropdownBtns.forEach(btn => 
     {
         btn.addEventListener("mouseenter", () => 
@@ -343,24 +273,24 @@ class ObjectInitializer
         {
             if (btn.classList.contains('changeSpeed'))
             {
-              torusRotationRate += 0.0025;
-              if (torusRotationRate > 0.0075)
-                torusRotationRate = 0.0025;
+              this.ShapeGeometryRotationRate += 0.0025;
+              if (this.ShapeGeometryRotationRate > 0.0075)
+                this.ShapeGeometryRotationRate = 0.0025;
 
-              torusGeometry.rotateZ(torusRotationRate);
+              particlesShapeGeometry.rotateZ(this.ShapeGeometryRotationRate);
 
             } else if (btn.classList.contains('changeColor'))
             {
               clearInterval(intervalId);
               colorIndex++;
               if (colorIndex > colors.length-1) colorIndex = 0;
-              setParticleColorsWithDelay(colorIndex);
+              this.SetParticleColorsWithDelay(colorIndex, particlesColors, colors, particlesGeometry);
             } else 
             {
               clearInterval(intervalId)
               intervalId = setInterval(() => 
               {
-                setParticleColorsWithDelay(colorIndex);
+                this.SetParticleColorsWithDelay(colorIndex, particlesColors, colors, particlesGeometry);
                 //setParticlesColorsWithoutDelay(colorIndex);
                 colorIndex++; 
                 if (colorIndex > colors.length-1) colorIndex = 0;
@@ -369,11 +299,72 @@ class ObjectInitializer
         });
     });
   }
+};
+
+class ParticleHelpers
+{
+  private Camera: THREE.PerspectiveCamera;
+  private Scene: THREE.Scene;
+  private Renderer: THREE.WebGLRenderer;
+
+  constructor(camera: THREE.PerspectiveCamera, scene: THREE.Scene, renderer: THREE.WebGLRenderer)
+  {
+    this.Camera = camera;
+    this.Scene = scene;
+    this.Renderer = renderer;
+  }
+
+  public GenerateShape(
+    geometry: THREE.BufferGeometry,
+    material: THREE.Material,
+    color: THREE.Color): THREE.Points
+  {
+    const geometryVertexPositions = geometry.getAttribute('position');
+    const particleCount: number = geometryVertexPositions.count*3;
+    const particlePositions: Float32Array = new Float32Array(particleCount);
+    const particlesColors: Float32Array = new Float32Array(particleCount);
+    const particlesGeometry = new THREE.BufferGeometry();
+    const vertex = new THREE.Vector3();
+
+    let j = 0;
+    for (let i=0; i<=particleCount; i+=3)
+    {
+      vertex.fromBufferAttribute(geometryVertexPositions, j);
+      particlePositions[i] = vertex.x;
+      particlePositions[i+1] = vertex.y;
+      particlePositions[i+2] = vertex.z;
+
+      particlesColors[i] = color.r;
+      particlesColors[i+1] = color.g;
+      particlesColors[i+2] = color.b;
+
+      j++;
+    }
+
+    particlesGeometry.setAttribute('position', new THREE.BufferAttribute(particlePositions, 3));
+    particlesGeometry.setAttribute('color', new THREE.BufferAttribute(particlesColors, 3));
+
+    const particles = new THREE.Points(particlesGeometry, material);
+    this.Scene.add(particles);
+
+    return particles;
+  }
 }
 
-const objectInitializer = new ObjectInitializer(setup.Scene, setup.Camera, setup.Renderer); 
+let shapeGeometryRotationRate = 0.0025;
+let cameraRotationRate = 0.0025;
 
-objectInitializer.ParticleShape();
-//objectInitializer.InstanceShape();
+const particlesHelper = new ParticleHelpers(
+  setup.Camera, 
+  setup.Scene, 
+  setup.Renderer);
+
+new Particles(
+  setup.Camera, 
+  setup.Scene, 
+  setup.Renderer, 
+  particlesHelper, 
+  shapeGeometryRotationRate, 
+  cameraRotationRate).Generate();
 
 
